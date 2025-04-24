67a30661d42f8accdcbbdd1c39913b5266c5699f
Chakib LAHNAKI <chakib@homeexchange.com>
2025-04-24 15:53:49 +0200

    create Movie Component and Mock data

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieComponent } from './movies/movie/movie.component';
 
 @Component({
   selector: 'app-root',
