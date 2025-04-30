47f3981017e5451b3d64fa56a8ce0e2de8553606
Chakib LAHNAKI <chakib.lahnaki@gmail.com>
2025-04-30 16:14:14 +0200

    connect to MongoDb & create Movie Model

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,27 @@
-import express from 'express';
-import movieRoutes from './routes/movies';
+import express from "express";
+import mongoose from "mongoose";
+
+import movieRoutes from "./routes/movies";
 
 const app = express();
 const port = 3000;
 
+mongoose
+  .connect("mongodb://localhost:27017/movie-app")
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`🚀 Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
+
 app.use(express.json());
 
-app.use('/api/movies', movieRoutes);
+app.use("/api/movies", movieRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
--- backend/src/routes/movies.ts
+++ backend/src/routes/movies.ts
@@ -1,61 +1,65 @@
 import { Router } from "express";
+import { Movie } from "../models/movie.model";
 
 const router = Router();
 
-const movies = [
-  { id: 1, title: "Inception", director: "Christopher Nolan", year: 2010 },
-  { id: 2, title: "Interstellar", director: "Christopher Nolan", year: 2014 },
-  { id: 3, title: "The Matrix", director: "Wachowskis", year: 1999 },
-];
+// const movies = [
+//   { id: 1, title: "Inception", director: "Christopher Nolan", year: 2010 },
+//   { id: 2, title: "Interstellar", director: "Christopher Nolan", year: 2014 },
+//   { id: 3, title: "The Matrix", director: "Wachowskis", year: 1999 },
+// ];
 
 // GET /api/movies -> List all movies
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const movies = await Movie.find();
   res.json(movies);
 });
 
 // GET /api/movies/:id -> Get one movie by ID
-router.get("/:id", (req, res) => {
-  const movieId = Number(req.params.id);
-  const movie = movies.find((m) => m.id === movieId);
+router.get("/:movieId", async (req, res) => {
+  const movieId = req.params.movieId;
+  //   const movie = movies.find((m) => m.id === movieId);
+  const movie = await Movie.findById(movieId);
   if (!movie) return res.status(404).json({ message: "Movie not found" });
   res.json(movie);
 });
 
 // POST /api/movies -> Add a new movie
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { title, director, year } = req.body;
-  const newMovie = {
-    id: movies.length + 1,
-    title,
-    director,
-    year,
-  };
-  movies.push(newMovie);
-  console.log(movies);
-  console.log(newMovie);
+  const newMovie = new Movie({ title, director, year });
+  await newMovie.save();
+  //   movies.push(newMovie);
   res.status(201).json(newMovie);
 });
 
 // PUT /api/movies/:id -> Update a movie
-router.put("/:id", (req, res) => {
-  const movieId = Number(req.params.id);
-  const index = movies.findIndex((m) => m.id === movieId);
-  if (index === -1) return res.status(404).json({ message: "Movie not found" });
+router.put("/:movieId", async (req, res) => {
+  const movieId = req.params.movieId;
+  //   const index = movies.findIndex((m) => m.id === movieId);
+  const updated = await Movie.findByIdAndUpdate(movieId, req.body, {
+    new: true,
+  });
+  if (!updated) return res.status(404).json({ message: "Movie not found" });
+  res.json(updated);
 
-  const { title, director, year } = req.body;
-  movies[index] = { ...movies[index], title, director, year };
-  console.log(movies[index]);
-  res.json(movies[index]);
+  //   if (index === -1) return res.status(404).json({ message: "Movie not found" });
+  //   const { title, director, year } = req.body;
+  //   movies[index] = { ...movies[index], title, director, year };
+  //   res.json(movies[index]);
 });
 
 // DELETE /api/movies/:id -> Delete a movie
-router.delete("/:id", (req, res) => {
-  const movieId = Number(req.params.id);
-  const index = movies.findIndex((m) => m.id === movieId);
-  if (index === -1) return res.status(404).json({ message: "Movie not found" });
+router.delete("/:movieId", async (req, res) => {
+  const movieId = req.params.movieId;
+  const deleted = await Movie.findByIdAndDelete(movieId);
+  if (!deleted) return res.status(404).json({ message: "Movie not found" });
+  res.json(deleted);
 
-  const deleted = movies.splice(index, 1);
-  res.json(deleted[0]);
+  //   const index = movies.findIndex((m) => m.id === movieId);
+  //   if (index === -1) return res.status(404).json({ message: "Movie not found" });
+  //   const deleted = movies.splice(index, 1);
+  //   res.json(deleted[0]);
 });
 
 export default router;
