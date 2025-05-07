443d02b2b1d621a5f6187de80b7056f83dacabd1
Chakib LAHNAKI <chakib@homeexchange.com>
2025-05-07 16:36:07 +0200

    create Http CRUD for movies

--- frontend/src/app/movies/movie.service.ts
+++ frontend/src/app/movies/movie.service.ts
@@ -1,12 +1,73 @@
-import { Injectable, signal } from '@angular/core';
-import { MOVIES } from './movie.data';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { catchError, map, throwError } from 'rxjs';
+import { ErrorService } from '../core/services/error.service';
+import { Movie } from './movie.model';
 
 @Injectable({ providedIn: 'root' })
 export class MovieService {
-  movies = signal(MOVIES);
-  allMovies = this.movies.asReadonly();
+  private http = inject(HttpClient);
+  private errorService = inject(ErrorService);
+  private baseUrl = `${environment.apiUrl}/movies`;
+
+  getAllMovies() {
+    return this.http.get<Movie[]>(this.baseUrl).pipe(
+      map((movies) =>
+        movies.map((movie) => ({
+          ...movie,
+          id: movie._id,
+        }))
+      ),
+      catchError((error) => this.handleError(error, 'Failed to fetch movies.'))
+    );
+  }
 
   getMovieById(id: string) {
-    return this.movies().filter((movie) => movie.id === id)[0];
+    console.log(id);
+    return this.http
+      .get<Movie>(`${this.baseUrl}/${id}`)
+      .pipe(
+        catchError((error) =>
+          this.handleError(error, 'Failed to fetch movies.')
+        )
+      );
+  }
+
+  addMovie(movie: Movie) {
+    return this.http
+      .post<Movie>(this.baseUrl, movie)
+      .pipe(
+        catchError((error) =>
+          this.handleError(error, 'Failed to fetch movies.')
+        )
+      );
+  }
+
+  updateMovie(movie: Movie) {
+    return this.http
+      .put(`${this.baseUrl}/${movie.id}`, movie)
+      .pipe(
+        catchError((error) =>
+          this.handleError(error, 'Failed to update movie.')
+        )
+      );
+  }
+
+  removeMovie(movie: Movie) {
+    return this.http
+      .delete(`${this.baseUrl}/${movie.id}`)
+      .pipe(
+        catchError((error) =>
+          this.handleError(error, 'Failed to remove movie.')
+        )
+      );
+  }
+
+  // Centralized error handler
+  private handleError(error: any, message: string) {
+    console.error(error);
+    this.errorService.showError(message);
+    return throwError(() => new Error(message));
   }
 }
